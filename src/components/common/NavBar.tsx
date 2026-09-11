95d0d4b818bee7bea36e01bb8e4cadcfa4a06c46
ChamlngUttam <uttamchamling51@gmail.com>
2026-09-11 16:11:38 +0545

    feat:update the colo of text and bg of navbar and faqs

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const navItems = ["Home", "Features", "Price", "Blog", "Contact"];
 
   return (
-    <nav className="w-full px-4 md:px-10 mt-5 text-white bg-[#3E1647]">
+    <nav className="w-full px-4 md:px-10 mt-5 text-white ">
       <div className="flex items-center justify-between max-w-7xl mx-auto py-4">
 
         {/* Logo */}
