d6024b5aeeb6c21147d82bde5673ac46ba6dae66
Sichan Shrestha <sichanstha1@gmail.com>
2026-09-17 11:22:18 +0545

    updated

---
+++ Services/api/schedule-demo.ts
@@ -0,0 +1,19 @@
+import api from "@/src/api/api";
+
+export type ScheduleDemoPayload = {
+  first_name: string;
+  last_name: string;
+  phone: string;
+  email: string;
+  organization_name: string;
+  institution_type: string;
+  capacity: number;
+  country: string;
+  address: string;
+  preferred_date: string;
+  preferred_time: string;
+  additional_note: string;
+};
+
+export const scheduleDemo = (data: ScheduleDemoPayload) =>
+  api.post("/public/shedule_demo", data);
